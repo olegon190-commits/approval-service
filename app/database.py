0ad2103b850e75dece87ec7d37e981586fee8f4d
olegon190-commits <danilov.olegon190@gmail.com>
2026-07-14 22:24:00 +0300

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,22 @@
+"""Подключение к базе данных"""
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./approval.db")
+
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
